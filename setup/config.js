26d4a88091ec9a495ac11a10fc9f681f8fa8bc29
Shashank <jfriday464@gmail.com>
2020-06-09 13:34:59 +0530

    Update config.js

--- setup/config.js
+++ setup/config.js
@@ -1,9 +1,3 @@
-// module.exports = {
-//    mongoURL:
-//       "mongodb+srv://friday:Hello123@stackoverflowclone-zcgsj.mongodb.net/test?retryWrites=true&w=majority",
-//    secret: "mystrongsecretdonottrytosteelit",
-// };
-
 module.exports = {
    mongoURL: process.env.mongoURL,
    secret: process.env.secret,
